f5668b587440cf550906f643305f986984dfa984
rayenfassatoui <rayenft2001@gmail.com>
2024-10-29 04:16:38 +0100

    add userid in interviews

--- actions/save-interview-data.ts
+++ actions/save-interview-data.ts
@@ -1,14 +1,21 @@
 "use server";
 
 import { prisma } from "@/lib/db";
-
+import { getCurrentUser } from "@/lib/session";
 export async function saveInterviewData(interviewData: any) {
+  const user = await getCurrentUser();
   try {
     const savedInterview = await prisma.interview.create({
+
       data: {
         jobTitle: interviewData.jobTitle,
         jobDescription: interviewData.jobDescription,
         interviewScore: interviewData.interviewScore || 0,
+        user: {
+          connect: {
+            id: user?.id
+          }
+        },
         interviewData: {
           create: interviewData.interviewData.map((item: any) => ({
             aiQuestion: item.aiQuestion,
