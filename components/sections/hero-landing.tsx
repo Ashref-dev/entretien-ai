645bf82f9fcd5e1140e5fab989593ecfceb1c444
rayenfassatoui <rayenft2001@gmail.com>
2024-11-06 20:39:38 +0100

    add linkedin balti :)

--- components/sections/hero-landing.tsx
+++ components/sections/hero-landing.tsx
@@ -27,7 +27,7 @@ export default async function HeroLanding() {
     <section className="space-y-6 py-12 sm:py-20 lg:py-20">
       <div className="container flex max-w-5xl flex-col items-center gap-5 text-center">
         <Link
-          href="https://ashref.tn"
+          href="https://www.linkedin.com/in/ahmedbalti/"
           className={cn(
             buttonVariants({ variant: "outline", size: "sm", rounded: "full" }),
             "px-4",
