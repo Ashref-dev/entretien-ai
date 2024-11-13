e9bd146380ddca7521ebd02dec74cd1cdf75c2fe
Mohamed Ashref Ben Abdallah <109633107+Ashref-dev@users.noreply.github.com>
2024-11-13 13:55:30 +0100

    Type fixes

--- auth.config.ts
+++ auth.config.ts
@@ -1,3 +1,4 @@
+import { NextAuthConfig } from "next-auth";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import Resend from "next-auth/providers/resend";
