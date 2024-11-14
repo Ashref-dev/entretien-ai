b382c4fc64342eed75a3397813436fd1f7f8c71e
Mohamed Ashref Ben Abdallah <109633107+Ashref-dev@users.noreply.github.com>
2024-11-14 19:36:53 +0100

    Added new components

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,6 +5,7 @@ import HeroLanding from "@/components/sections/hero-landing";
 import InfoLanding from "@/components/sections/info-landing";
 import LogoCloud from "@/components/sections/logo-cloud";
 import PreviewLanding from "@/components/sections/preview-landing";
+import ProductShowcase from "@/components/sections/product-showcase";
 import Testimonials from "@/components/sections/testimonials";
 
 export default function IndexPage() {
@@ -18,6 +19,7 @@ export default function IndexPage() {
       <InfoLanding data={infos[0]} reverse={true} />
       <InfoLanding data={infos[1]} />
       <Features />
+      <ProductShowcase />
       <Testimonials />
     </>
   );
