cfeda2be6b4fb8c29d6d06b1d90ddc4f67e56ced
rayenfassatoui <rayenft2001@gmail.com>
2024-11-15 20:50:44 +0100

    retate 15deg

--- components/sections/Compare.tsx
+++ components/sections/Compare.tsx
@@ -16,7 +16,7 @@ export function CompareDemo() {
       <div className="flex items-center justify-center h-[80vh] [perspective:800px] [transform-style:preserve-3d]">
         <div
           style={{
-            transform: "rotateX(1deg) translateZ(80px)",
+            transform: "rotateX(15deg) translateZ(80px)",
           }}
           className="p-1 md:p-4 border rounded-3xl dark:bg-neutral-900 bg-neutral-100 border-neutral-200 dark:border-neutral-800 w-full max-w-5xl h-3/4 md:h-[85%]"
         >
