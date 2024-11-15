5402005850978c3d80758db833f05b2329c8bb64
rayenfassatoui <rayenft2001@gmail.com>
2024-11-15 20:48:27 +0100

    fix error autoplayref

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,6 @@
 import { infos } from "@/config/landing";
 import BentoGrid from "@/components/sections/bentogrid";
+import { CompareDemo } from "@/components/sections/Compare";
 import Features from "@/components/sections/features";
 import HeroLanding from "@/components/sections/hero-landing";
 import InfoLanding from "@/components/sections/info-landing";
@@ -7,7 +8,7 @@ import LogoCloud from "@/components/sections/logo-cloud";
 import ProductShowcase from "@/components/sections/product-showcase";
 import HeroScroll from "@/components/sections/scroll-animation";
 import Testimonials from "@/components/sections/testimonials";
-import { CompareDemo } from "@/components/sections/Compare";
+
 export default function IndexPage() {
   return (
     <>
@@ -15,15 +16,13 @@ export default function IndexPage() {
       <HeroScroll />
       {/* <PreviewLanding /> */}
       {/* <Powered /> */}
-      
+
       <LogoCloud />
       <BentoGrid />
       <InfoLanding data={infos[0]} reverse={true} />
       <CompareDemo />
       <InfoLanding data={infos[1]} />
-     
       <Features />
-     
       <ProductShowcase />
       <Testimonials />
     </>
