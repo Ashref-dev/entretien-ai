c887c7db3f7602f822e8b18d7c32e3f5ecc5c05c
Mohamed Ashref Ben Abdallah <109633107+Ashref-dev@users.noreply.github.com>
2024-12-30 19:12:24 +0100

    Refactor authentication and email handling, update dependencies, and clean up assets
    - Enabled email sign-in functionality in the SignInModal component, allowing users to receive magic links for authentication.
    - Updated the MagicLinkEmail component to enhance the email template with improved styling and content.
    - Modified the sendVerificationRequest function to handle email sending more robustly, including better error logging.
    - Added support for new email components in the auth.config.ts file.
    - Updated package.json to reflect new versions of dependencies, including @prisma/client and resend.
    - Removed outdated avatar and blog images from the public directory to clean up unused assets.
    
    These changes aim to improve user authentication experience, enhance email communication, and maintain a cleaner project structure.

--- auth.config.ts
+++ auth.config.ts
@@ -4,23 +4,24 @@ import Google from "next-auth/providers/google";
 import Resend from "next-auth/providers/resend";
 
 import { env } from "@/env.mjs";
-
-// import { sendVerificationRequest } from "@/lib/email";
+import { sendVerificationRequest } from "@/lib/email";
 
 export default {
   providers: [
     Google({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     Github({
       clientId: env.AUTH_GITHUB_ID,
       clientSecret: env.AUTH_GITHUB_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     Resend({
       apiKey: env.RESEND_API_KEY,
       from: env.EMAIL_FROM,
-      // sendVerificationRequest,
+      sendVerificationRequest,
     }),
   ],
 } satisfies NextAuthConfig;
--- prisma/migrations/20241121152524_added_status/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "interviews" ADD COLUMN     "errorMessage" TEXT,
-ADD COLUMN     "status" TEXT NOT NULL DEFAULT 'PROCESSING';
