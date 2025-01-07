f97917ee04373a882ee2f3e736733fed14a25b51
Mohamed Ashref Ben Abdallah <109633107+Ashref-dev@users.noreply.github.com>
2025-01-07 13:17:44 +0100

    Refactor login page and user authentication form for improved user experience

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,4 +1,3 @@
-import { Suspense } from "react";
 import { Metadata } from "next";
 import Link from "next/link";
 
@@ -9,7 +8,7 @@ import { Icons } from "@/components/shared/icons";
 
 export const metadata: Metadata = {
   title: "Login",
-  description: "Login to your account",
+  description: "Login to your account or create an account",
 };
 
 export default function LoginPage() {
@@ -30,24 +29,13 @@ export default function LoginPage() {
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
           <Icons.logo className="mx-auto size-10" />
-          <h1 className="text-2xl font-semibold tracking-tight">
-            Welcome back
-          </h1>
+          <h1 className="text-2xl font-semibold tracking-tight">Welcome</h1>
           <p className="text-sm text-muted-foreground">
-            Enter your email to sign in to your account
+            Enter your email to sign in or create an account automatically
           </p>
         </div>
-        <Suspense>
-          <UserAuthForm />
-        </Suspense>
-        {/* <p className="px-8 text-center text-sm text-muted-foreground">
-          <Link
-            href="/register"
-            className="hover:text-brand underline underline-offset-4"
-          >
-            Don&apos;t have an account? Sign Up
-          </Link>
-        </p> */}
+
+        <UserAuthForm />
       </div>
     </div>
   );
